b1af0f76e517bea19457c32f3809c0515673694f
Todd Brannon <toddqbrannon@gmail.com>
2020-12-20 14:04:47 -0600

    added exclamation point

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        Deployment is happening here
+        Deployment is happening here!
       </header>
     </div>
   );
